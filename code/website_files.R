1e1a0691c935d64caf4f229f97b2d50328d32a05
jaspershen <shenxt1990@outlook.com>
2023-04-14 23:13:13 -0700

    Committing local changes

---
+++ code/website_files.R
@@ -0,0 +1,6 @@
+# distill::create_website(dir = "website_files/", gh_pages = TRUE)
+masstools::setwd_project()
+##render websites
+rmarkdown::render_site(input = "website_files/")
+##copy website docs to root folder
+file.copy(from = "website_files/docs/", to = ".", recursive = TRUE, overwrite = TRUE)
